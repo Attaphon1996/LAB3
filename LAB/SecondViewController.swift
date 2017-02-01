704ee177861a0409c20292a681c8aea4ff27f8d6
อรรถพล วงษ์วิกิจการ <boatbright00@gmail.com>
2017-02-01 16:23:40 +0700

    Initial Commit

---
+++ LAB/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LAB
+//
+//  Created by อรรถพล วงษ์วิกิจการ on 2/1/2560 BE.
+//  Copyright © 2560 อรรถพล วงษ์วิกิจการ. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
